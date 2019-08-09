eb47824fd579ca63c3e522bffa90ec9782a50b9a
chichiglacierz <tytrainor@hotmail.com>
2019-08-09 04:05:02 +0200

    completed task nr. 2

--- Week2/homework/maartjes-work.js
+++ Week2/homework/maartjes-work.js
@@ -45,9 +45,17 @@ const tuesday = [
 const maartjesTasks = monday.concat(tuesday);
 const maartjesHourlyRate = 20;
 
+/**
+ * Use of High order functions: map, filter, toFixed
+ */
+
 function computeEarnings(tasks, hourlyRate) {
-  // Replace this comment and the next line with your code
-  console.log(tasks, hourlyRate);
+  // mapping duration of tasks from minutes to hours and filtering out duties < 2h
+  const timeInHours = tasks.map(time => time.duration / 60).filter(duty => duty >= 2);
+  const salaryPerDay = timeInHours.map(hoursPerDay => hoursPerDay * hourlyRate);
+  const totalSalary = salaryPerDay.reduce((sum, dailySalary) => sum + dailySalary, 0);
+  const euroSalary = totalSalary.toFixed(2);
+  return euroSalary;
 }
 
 // eslint-disable-next-line no-unused-vars
@@ -55,7 +63,7 @@ const earnings = computeEarnings(maartjesTasks, maartjesHourlyRate);
 
 // add code to convert `earnings` to a string rounded to two decimals (euro cents)
 
-console.log(`Maartje has earned €${'replace this string with the earnings rounded to euro cents'}`);
+console.log(`Maartje has earned €${earnings}`);
 
 // Do not change or remove anything below this line
 module.exports = {
--- Week2/homework/map-filter.js
+++ Week2/homework/map-filter.js
@@ -1,12 +1,16 @@
 'use strict';
-
 function doubleOddNumbers(numbers) {
   // Replace this comment and the next line with your code
+  // the following iterates the variables in numbers(with filter) and checking if they´re divisible by 2
+  // const newNumbers = [];
+  // for (let i = 0; i =numbers.length)
+  numbers = numbers.filter(num => num % 2 === 1).map(num => num * 2);
   console.log(numbers);
+  return numbers;
 }
 
 const myNumbers = [1, 2, 3, 4];
-console.log(doubleOddNumbers(myNumbers));
+console.log(doubleOddNumbers(myNumbers)); // output : [2, 6]
 
 // Do not change or remove anything below this line
 module.exports = {
